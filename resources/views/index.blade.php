155b644e5d35f4dc8cae6017efcc68d2c3394e37
mark anthony valencia <valmarkencia@gmail.com>
2017-03-21 19:03:23 +0800

    bitbucket repo

---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('content')
+  <nav class="white-bg">
+  	<div class="container">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+    </div>
+  </nav>
+
+
+ <div class="preloader-wrapper big active">
+    <div class="spinner-layer spinner-blue-only">
+      <div class="circle-clipper left">
+        <div class="circle"></div>
+      </div><div class="gap-patch">
+        <div class="circle"></div>
+      </div><div class="circle-clipper right">
+        <div class="circle"></div>
+      </div>
+    </div>
+  </div>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/4.0.2/bootstrap-material-design.iife.js"></script>
+
+  <script type="text/javascript">
+  	$(function() {
+	  $('.preloader-wrapper').css('display', 'none');
+	});
+  </script>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Premium Online Properties</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
