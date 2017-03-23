c14fa08f6f902ab77c43b0ee55facf9ca089fc1f
mark anthony valencia <valmarkencia@gmail.com>
2017-03-24 02:16:04 +0800

    banner and class of own

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,37 +1,70 @@
 @extends('layouts.master')
 
 @section('content')
-  <nav class="white-bg">
-  	<div class="container">
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-    </div>
-  </nav>
+<div class="jumbotron banner" style="background-image: url(/img/banner.jpg);">
+	<div class="container">
+		<div class="search">
+			<form>
+			<h1 class="banner-title">BOTANIKA</h1>
+			<div class="form-inline">
+			  <div class="form-group">
+			    <select class="form-control first" id="bedroom" placeholder="Email">
+			    <option selected="selected">No. of Bedroom</option>
+			    <option></option>
+			    <option></option>
+			    </select>
+			  </div
+			  ><div class="form-group">
+			    <select class="form-control middle" id="bathroom" placeholder="Email">
+			    <option selected="selected">No. of Bathroom</option>
+			    <option></option>
+			    <option></option>
+			    </select>
+			  </div
+			  ><div class="form-group">
+			    <select class="form-control middle" id="price" placeholder="Email">
+			    <option selected="selected">Price Range</option>
+			    <option></option>
+			    <option></option>
+			    </select>
+			  </div
+			  ><div class="form-group">
+			    <select class="form-control last" id="size" placeholder="Email">
+			    <option selected="selected">Unit Size</option>
+			    <option></option>
+			    <option></option>
+			    </select>
+			  </div>
+			  </div>
+			  <button type="submit" class="btn search-btn">Search</button>
+			</form>
+		</div>
+	</div>
+</div>
+</div><!-- this is for the 100vh section -->
 
 
- <div class="preloader-wrapper big active">
-    <div class="spinner-layer spinner-blue-only">
-      <div class="circle-clipper left">
-        <div class="circle"></div>
-      </div><div class="gap-patch">
-        <div class="circle"></div>
-      </div><div class="circle-clipper right">
-        <div class="circle"></div>
-      </div>
-    </div>
-  </div>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/4.0.2/bootstrap-material-design.iife.js"></script>
 
-  <script type="text/javascript">
-  	$(function() {
-	  $('.preloader-wrapper').css('display', 'none');
-	});
-  </script>
+
+<!-- after banner -->
+<div class="section class-of-own">
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<img src="/img/class-of-own.jpg">
+		</div>
+		<div class="col-md-6">
+			<h2 class="content-title">A Class of Its Own</h2>
+			<p class="content">
+				What do you do when the property you’re building just doesn’t seem to fit the rest because it’s simply like no other? You create a special universe for it. <br><br>
+
+				That’s the vision for Botanika Nature Residences, under the Exclusive Collection, a new category in the Filinvest Group’s roster of properties, one-of-a-kind residential spaces will come to life.<br><br>
+
+				Botanika embodies Green Living in a vertical home with that genuine village charm that feels closer to the Filipino hearts. Located at the finest locale of Filinvest City, Botanika is truly a refreshing new address to the privileged few.
+			</p>
+		</div>
+	</div>
+</div>
+</div>
+
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,10 +3,38 @@
 <head>
 	<title>Premium Online Properties</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+<div class="full-height">
+  <nav class="navbar navbar-default navbar-fixed-top white-bg">
+  <div class="container nav-content">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="/img/logoOnly.png"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="navigation-bar collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a>
+        <li><a href="#">Properties</a>
+        <li><a href="#">Contact</a>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 	@yield('content')
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
