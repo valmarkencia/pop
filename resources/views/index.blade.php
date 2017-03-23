3d334ce947bf7aeebea01d024bec2767ce99de45
mark anthony valencia <valmarkencia@gmail.com>
2017-03-24 06:55:11 +0800

    homepage fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -151,8 +151,8 @@
 <div class="masterpiece">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-7"></div>
-			<div class="col-md-5 master-content">
+			<div class="col-md-8"></div>
+			<div class="col-md-4 master-content">
 				<h2 class="content-title">
 					Impeccable Masterpieces
 				</h2>
